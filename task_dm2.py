249100c9aa0b7b035d070507627f5dbdbd584156
Таня <rykuntv@mail.ru>
2016-07-13 01:01:32 +0300

    Исправлена ошибка изменения масштаба

--- task_dm2.py
+++ task_dm2.py
@@ -3,12 +3,13 @@ from time import sleep
 
 def f(x):
     return x*x
-drawman_scale=20
+
+drawman_scale(40)
 x=-5.0
-to_point(x, f(x))
+to_point (x, f(x))
 pen_down()
 while  x  <= 5:
-    to_point(x, f(x))
+    to_point (x, f(x))
     x += 0.1
 pen_up()
 
